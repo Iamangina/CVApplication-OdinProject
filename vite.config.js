5457358200789346b4f494f685c6ef0d18cf5c60
Iamangina <anginaart@gmail.com>
2026-01-07 13:57:13 +0000

    Add base path for CVApplication-OdinProject

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/CVApplication-OdinProject/',
+  plugins: [react()],
+});
 
 // https://vite.dev/config/
 export default defineConfig({
