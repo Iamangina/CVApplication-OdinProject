8fed2e3f30291280db94a531035f35000bf85eea
Iamangina <anginaart@gmail.com>
2026-01-07 17:10:27 +0000

    Fix vite.config.js for build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/CVApplication-OdinProject/',
-  plugins: [react()],
-});
 
 // https://vite.dev/config/
 export default defineConfig({
